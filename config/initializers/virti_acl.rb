220cca2e613793ee902f5d5d2181723360c67f5f
Jose Ulisses <joseulisses03@gmail.com>
2026-06-24 14:41:07 -0300

    Filtra update de presenca de contatos no ActionCable

--- config/initializers/virti_acl.rb
+++ config/initializers/virti_acl.rb
@@ -1,29 +1,32 @@
 Rails.application.config.to_prepare do
   permission_filter_patch = Virti::Acl::Patches::PermissionFilterServicePatch
   permission_filter_service = Conversations::PermissionFilterService
-  permission_filter_service.prepend(permission_filter_patch) unless permission_filter_service.ancestors.include?(permission_filter_patch)
+  permission_filter_service.prepend(permission_filter_patch) unless permission_filter_patch <= permission_filter_service
 
   conversations_patch = Virti::Acl::Patches::ConversationsControllerPatch
   conversations_controller = Api::V1::Accounts::ConversationsController
-  conversations_controller.prepend(conversations_patch) unless conversations_controller.ancestors.include?(conversations_patch)
+  conversations_controller.prepend(conversations_patch) unless conversations_patch <= conversations_controller
 
   conversations_base_patch = Virti::Acl::Patches::ConversationsBaseControllerPatch
   conversations_base_controller = Api::V1::Accounts::Conversations::BaseController
-  conversations_base_controller.prepend(conversations_base_patch) unless conversations_base_controller.ancestors.include?(conversations_base_patch)
+  conversations_base_controller.prepend(conversations_base_patch) unless conversations_base_patch <= conversations_base_controller
 
   assignments_patch = Virti::Acl::Patches::AssignmentsControllerPatch
   assignments_controller = Api::V1::Accounts::Conversations::AssignmentsController
-  assignments_controller.prepend(assignments_patch) unless assignments_controller.ancestors.include?(assignments_patch)
+  assignments_controller.prepend(assignments_patch) unless assignments_patch <= assignments_controller
 
   notification_builder_patch = Virti::Acl::Patches::NotificationBuilderPatch
-  NotificationBuilder.prepend(notification_builder_patch) unless NotificationBuilder.ancestors.include?(notification_builder_patch)
+  NotificationBuilder.prepend(notification_builder_patch) unless notification_builder_patch <= NotificationBuilder
 
   notification_finder_patch = Virti::Acl::Patches::NotificationFinderPatch
-  NotificationFinder.prepend(notification_finder_patch) unless NotificationFinder.ancestors.include?(notification_finder_patch)
+  NotificationFinder.prepend(notification_finder_patch) unless notification_finder_patch <= NotificationFinder
 
   action_cable_broadcast_job_patch = Virti::Acl::Patches::ActionCableBroadcastJobPatch
-  ActionCableBroadcastJob.prepend(action_cable_broadcast_job_patch) unless ActionCableBroadcastJob.ancestors.include?(action_cable_broadcast_job_patch)
+  ActionCableBroadcastJob.prepend(action_cable_broadcast_job_patch) unless action_cable_broadcast_job_patch <= ActionCableBroadcastJob
 
   action_cable_listener_patch = Virti::Acl::Patches::ActionCableListenerPatch
-  ActionCableListener.prepend(action_cable_listener_patch) unless ActionCableListener.ancestors.include?(action_cable_listener_patch)
+  ActionCableListener.prepend(action_cable_listener_patch) unless action_cable_listener_patch <= ActionCableListener
+
+  room_channel_patch = Virti::Acl::Patches::RoomChannelPatch
+  RoomChannel.prepend(room_channel_patch) unless room_channel_patch <= RoomChannel
 end
--- spec/channels/room_channel_spec.rb
+++ spec/channels/room_channel_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe RoomChannel do
 
   it 'subscribes to a stream when pubsub_token is provided' do
     subscribe(pubsub_token: contact_inbox.pubsub_token)
+
     expect(subscription).to be_confirmed
     expect(subscription).to have_stream_for(contact_inbox.pubsub_token)
   end
@@ -20,5 +21,85 @@ RSpec.describe RoomChannel do
     expect(subscription).to be_confirmed
     expect(subscription).to have_stream_for(user.pubsub_token)
     expect(subscription).to have_stream_for("account_#{account.id}")
+  end
+
+  it 'broadcasts only online contact ids authorized by Virti ACL in presence.update' do
+    presence = prepare_presence_acl_scenario
+    expected_contacts = { presence[:visible_contact].id.to_s => 'online' }
+
+    log_action_cable_characterization(
+      'presence.update',
+      user.pubsub_token,
+      "contacts=#{expected_contacts.keys.inspect} hidden_contact_id=#{presence[:hidden_contact].id} filtered"
+    )
+
+    expect(ActionCable.server).to receive(:broadcast).with(
+      user.pubsub_token,
+      {
+        event: 'presence.update',
+        data: { account_id: account.id, users: presence[:users], contacts: expected_contacts }
+      }
+    )
+
+    subscribe(user_id: user.id, pubsub_token: user.pubsub_token, account_id: account.id)
+  end
+
+  it 'keeps all online contact ids in presence.update when Virti ACL is disabled' do
+    presence = prepare_presence_acl_scenario
+
+    expect(ActionCable.server).to receive(:broadcast).with(
+      user.pubsub_token,
+      {
+        event: 'presence.update',
+        data: { account_id: account.id, users: presence[:users], contacts: presence[:contacts] }
+      }
+    )
+
+    with_modified_env VIRTI_ACL_ENABLED: 'false' do
+      subscribe(user_id: user.id, pubsub_token: user.pubsub_token, account_id: account.id)
+    end
+  end
+
+  def prepare_presence_acl_scenario
+    inbox = create(:inbox, account: account)
+    other_agent = create(:user, account: account, role: :agent)
+    create(:inbox_member, inbox: inbox, user: user)
+    create(:inbox_member, inbox: inbox, user: other_agent)
+    visible_contact, hidden_contact = create_presence_contacts(inbox, other_agent)
+    restrict_conversation_acl(user)
+
+    stub_presence(visible_contact, hidden_contact)
+  end
+
+  def create_presence_contacts(inbox, other_agent)
+    visible_contact = create(:contact, account: account)
+    hidden_contact = create(:contact, account: account)
+    create(:conversation, account: account, inbox: inbox, contact: visible_contact, assignee: user)
+    create(:conversation, account: account, inbox: inbox, contact: hidden_contact, assignee: other_agent)
+
+    [visible_contact, hidden_contact]
+  end
+
+  def stub_presence(visible_contact, hidden_contact)
+    contacts_presence = { visible_contact.id.to_s => 'online', hidden_contact.id.to_s => 'online' }
+    users_presence = { user.id.to_s => 'online' }
+    allow(OnlineStatusTracker).to receive(:update_presence)
+    allow(OnlineStatusTracker).to receive(:get_available_users).with(account.id).and_return(users_presence)
+    allow(OnlineStatusTracker).to receive(:get_available_contacts).with(account.id).and_return(contacts_presence)
+
+    { contacts: contacts_presence, hidden_contact: hidden_contact, users: users_presence, visible_contact: visible_contact }
+  end
+
+  def restrict_conversation_acl(user)
+    model = create(
+      :virti_acl_model,
+      account: account,
+      permissions: { 'pode_ver_aba_de_todas_conversas' => false, 'pode_ver_aba_de_nao_atribuidas' => false }
+    )
+    create(:virti_acl_user_model, account: account, user: user, model: model)
+  end
+
+  def log_action_cable_characterization(event_name, token, details)
+    Rails.logger.info("[Virti ACL characterization] #{event_name} ActionCable token=#{token.inspect} #{details}")
   end
 end
